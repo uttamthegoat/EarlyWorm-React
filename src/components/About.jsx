308c6427e678a12d839041d5a6f357858ecad3b0
uttamthegoat <19516uttam@gmail.com>
2023-05-30 23:28:07 +0530

    Update after changing class based components to function based components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,55 @@
-import React from 'react'
-import profilepic from "./images/botaniz_uttam.jpeg"
+import React, { useEffect } from "react";
+import profilepic from "./images/botaniz_uttam.jpeg";
 
-export default function About(props) {
-  const linkStyle={
-    color:props.theme==="light"?"black":"white",
-    textDecoration:"none"
+export default function About (props) {
+  let linkStyle = {
+    color: props.theme === "light" ? "black" : "white",
+    textDecoration: "none",
+  };
+  useEffect(() => {
+    props.setProgress(10)
+    setTimeout(() => {
+      props.setProgress(100)
+    }, 1000);
+  }, [])
+    return (
+      <div className="container">
+        <h1 className="text-center my-4">About</h1>
+        <div className="about__intro my-4">
+          <p className="fs-5">
+            NewsMonkey is a news app. It is a React.js project developed using
+            api from newsapi.org. It is a project developed during the initial
+            day of me learning React.js. It gives current affairs of India.
+            Based on the many categories like Business, Entertainment, General,
+            Health, Science, Sports, Technology and many more.
+          </p>
+        </div>
+        <div className="about__profile">
+          <img
+            src={profilepic}
+            alt="myPic"
+            width={200}
+            className="mx-auto d-block rounded-4 about__profilepic"
+          />
+          <h1
+            className={`text-${
+              props.theme === "light" ? "muted" : "light"
+            } text-center`}
+          >
+            Uttam
+          </h1>
+          <div
+            className="text-center border border-2 border-primary mx-auto"
+            style={{ width: "111px" }}
+          ></div>
+          <p className="fs-5">
+            I am a student currently pursuing Computer Science & Engineering at{" "}
+            <a href="https://mite.ac.in/" style={linkStyle}>
+              Mangalore Institute of Technology and Engineering, Moodbidri
+            </a>
+            . I aspire to become a good developer.
+          </p>
+        </div>
+      </div>
+    );
   }
-  return (
-    <div className='container'>
-      <h1 className='text-center my-4'>About</h1>
-      <div className="about__intro my-4">
-        <p className='fs-5'>NewsMonkey is a news app. It is a React.js project developed using api from newsapi.org. It is a project developed during the initial day of me learning React.js. It gives current affairs of India. Based on the many categories like Business, Entertainment, General, Health, Science, Sports, Technology and many more.</p>
-      </div>
-      <div className="about__profile">
-        <img src={profilepic} alt="myPic" width={200} className='mx-auto d-block rounded-4 about__profilepic'/>
-        <h1 className={`text-${props.theme==="light"?"muted":"light"} text-center`}>Uttam</h1>
-        <div className="text-center border border-2 border-primary mx-auto" style={{width:"111px"}}></div>
-        <p className='fs-5'>I am a student currently pursuing Computer Science & Engineering at <a href='https://mite.ac.in/' style={linkStyle}>Mangalore Institute of Technology and Engineering, Moodbidri</a>. I aspire to become a good developer.</p>
-      </div>
-    </div>
-  )
-}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,13 @@ export default function Home(props) {
   return (
     <div className='home'>
         <div className='home__rows'>
-          <Row pageSize={20} country="in" category="business"/>
-          <Row pageSize={20} country="in" category="entertainment"/>
-          <Row pageSize={20} country="in" category="general"/>
-          <Row pageSize={20} country="in" category="health"/>
-          <Row pageSize={20} country="in" category="science"/>
-          <Row pageSize={20} country="in" category="sports"/>
-          <Row pageSize={20} country="in" category="technology"/>
+          <Row apiKey={props.apiKey} setProgress={props.setProgress} pageSize={20} country="in" category="business"/>
+          <Row apiKey={props.apiKey} setProgress={props.setProgress} pageSize={20} country="in" category="entertainment"/>
+          <Row apiKey={props.apiKey} setProgress={props.setProgress} pageSize={20} country="in" category="general"/>
+          <Row apiKey={props.apiKey} setProgress={props.setProgress} pageSize={20} country="in" category="health"/>
+          <Row apiKey={props.apiKey} setProgress={props.setProgress} pageSize={20} country="in" category="science"/>
+          <Row apiKey={props.apiKey} setProgress={props.setProgress} pageSize={20} country="in" category="sports"/>
+          <Row apiKey={props.apiKey} setProgress={props.setProgress} pageSize={20} country="in" category="technology"/>
         </div>
     </div>
   )
